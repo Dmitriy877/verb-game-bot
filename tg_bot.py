58e362c785b9fbf576b8971b1ee4780119b0099e
Dmitriy877 <dmitriybukin05@gmail.com>
2025-10-09 22:10:47 +0300

    Add dialogflow
    Add dialogflow

--- tg_bot.py
+++ tg_bot.py
@@ -20,25 +20,63 @@ def echo(update: Update, context: CallbackContext) -> None:
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    from google.cloud import dialogflow
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    for text in texts:
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+
+        print("=" * 20)
+        print("Query text: {}".format(response.query_result.query_text))
+        print(
+            "Detected intent: {} (confidence: {})\n".format(
+                response.query_result.intent.display_name,
+                response.query_result.intent_detection_confidence,
+            )
+        )
+        print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+
+
 def main():
 
     env.read_env()
-    telegram_bot_token = env.str('TELEGRAM_BOT_TOKEN')
+    project_id = env.str("PROJECT_ID")
+    session_id = env.str("SESSION_ID")
+    texts = ['Привет!']
+    language_code = "ru-RU"
+    detect_intent_texts(project_id, session_id, texts, language_code)
 
-    updater = Updater(token=telegram_bot_token)
-    dispatcher = updater.dispatcher
+    # telegram_bot_token = env.str('TELEGRAM_BOT_TOKEN')
 
-    logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=logging.INFO
-    )
+    # updater = Updater(token=telegram_bot_token)
+    # dispatcher = updater.dispatcher
 
-    start_handler = CommandHandler('start', start)
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    # logging.basicConfig(
+    #     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    #     level=logging.INFO
+    # )
 
-    dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(echo_handler)
-    updater.start_polling()
+    # start_handler = CommandHandler('start', start)
+    # echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+
+    # dispatcher.add_handler(start_handler)
+    # dispatcher.add_handler(echo_handler)
+    # updater.start_polling()
 
 
 if __name__ == '__main__':
