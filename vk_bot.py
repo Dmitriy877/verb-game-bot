4b310b5bb4dec79dc4d52cabe8e178d230ecbfa5
Dmitriy877 <dmitriybukin05@gmail.com>
2025-10-28 22:25:45 +0300

    Add: Vk bot dont answer unclear message
    Add: Vk bot dont answer unclear message

--- vk_bot.py
+++ vk_bot.py
@@ -8,22 +8,23 @@ from dialogflow_bot import detect_intent_texts
 env.read_env()
 PROJECT_ID = env.str("Project_ID")
 LANGUAGE_CODE = "en-US"
+VK_API_KEY = env.str('VK_API_KEY')
 
 
 def echo(event, vk_api):
     session_id = event.user_id
     text = [event.text]
     answer = detect_intent_texts(PROJECT_ID, session_id, text, LANGUAGE_CODE)
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=answer,
-        random_id=random.randint(1, 1000)
-    )
+    if answer:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=answer,
+            random_id=random.randint(1, 1000)
+        )
 
 
 def main():
 
-    env.read_env()
     VK_API_KEY = env.str('VK_API_KEY')
 
     vk_session = vk.VkApi(token=VK_API_KEY)
