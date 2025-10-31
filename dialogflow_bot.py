7cc3c8a2eb3918d5aba19f083bf73bfc150634a7
Dmitriy877 <dmitriybukin05@gmail.com>
2025-10-31 20:29:19 +0300

    Fix imports
    Fix imports

--- dialogflow_bot.py
+++ dialogflow_bot.py
@@ -1,9 +1,11 @@
+from google.cloud import dialogflow
+
+
 def detect_intent_texts(project_id: str, session_id: int, texts: list, language_code: str) -> str:
     """Returns the result of detect intent with texts as inputs.
 
     Using the same `session_id` between requests allows continuation
     of the conversation."""
-    from google.cloud import dialogflow
 
     session_client = dialogflow.SessionsClient()
 
